ec772c4d48436e8cb61b3597e11870540e41882c
Astley Santos <astley_394@hotmail.com>
2026-07-26 20:03:51 +0200

    added springboot base backend configuration

---
+++ backend/src/main/java/com/ums/ums_backend/UmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ums.ums_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmsBackendApplication.class, args);
+	}
+
+}
